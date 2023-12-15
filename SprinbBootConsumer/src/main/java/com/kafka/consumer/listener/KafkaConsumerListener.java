5c3bf92531942ac4e44f5a324282f65c8b4977ba
Jaime <jaisan@gmail.com>
2023-12-15 23:49:23 +0100

    Configuration of Consumer

---
+++ SprinbBootConsumer/src/main/java/com/kafka/consumer/listener/KafkaConsumerListener.java
@@ -0,0 +1,16 @@
+package com.kafka.consumer.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConsumerListener {
+    private Logger LOGGER = LoggerFactory.getLogger(KafkaConsumerListener.class);
+
+    @KafkaListener(topics = {"topic-in-spring-boot"}, groupId = "my-group-id")
+    public void listener(String message) {
+        LOGGER.info("Mensaje recibido, el mesaje es {}", message);
+    }
+}
